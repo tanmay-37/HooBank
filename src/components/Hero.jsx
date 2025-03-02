cdf7cb90305350f41cdcc19208d79931982e2507
tanmay-37 <talekartk@rknec.edu>
2025-03-02 19:55:20 +0530

    business section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,6 +38,18 @@ const Hero = () => {
           annual fees.
         </p>
       </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} relative md:my-0 my-10`}>
+        <img src={robot} alt="biling" className='w-[100%] h-[100%] z-[5]' />
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-0 white__gradient'/>
+        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`} >
+        <GetStarted />
+      </div>
+
     </section>
   );
 }
